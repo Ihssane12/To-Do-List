924b8b28593f9783130c12e15cb8b3d33191d43f
Ihssane Christien <ihssanechristien@gmail.com>
2025-01-30 13:03:16 +0100

    esline

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,26 @@
 import { Linter } from "eslint";
+import eslintRecommended from "eslint/conf/eslint-recommended";
+import typescriptEslintRecommended from "@typescript-eslint/eslint-plugin/dist/configs/recommended";
+import prettierRecommended from "eslint-config-prettier";
 
-const config = {
-  languageOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-    globals: {
-      browser: true,
-      es2021: true,
-      node: true,
+const config = [
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        browser: true,
+        es2021: true,
+        node: true,
+      },
+    },
+    rules: {
+      // Add your custom rules here
     },
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-  ],
-  rules: {
-    // Add your custom rules here
-  },
-};
+  eslintRecommended,
+  typescriptEslintRecommended,
+  prettierRecommended,
+];
 
 export default config;
